e19f993e99b35538a85b81eb73a9d9d8c559664f
Jeongyong Lee <hua9378@naver.com>
2022-02-07 17:27:09 +0900

    함수 순서 변경

--- chap05_test01/main.c
+++ chap05_test01/main.c
@@ -6,6 +6,7 @@
 
 // <반환자료형> <함수명>(<파라미터>){}
 
+/*
 int sum(int x, int y)
 {
 	if (x < 0) x = x * -1; // 절대값
@@ -14,6 +15,9 @@ int sum(int x, int y)
 	int result = x + y;
 	return result; // 함수 종료 선언, 결과값 반환
 }
+*/
+int sum(int, int); // main보다 함수가 밑에 있을 때, 함수 선언
+
 
 int main(void)
 {
@@ -29,4 +33,14 @@ int main(void)
 	printf("> -2와 -27의 절대값 합은 %d입니다.\n", sum_result);
 
 	return 0; // 정상종료:0
+}
+
+
+int sum(int x, int y)
+{
+	if (x < 0) x = x * -1; // 절대값
+	if (y < 0) y = y * -1;
+
+	int result = x + y;
+	return result; // 함수 종료 선언, 결과값 반환
 }

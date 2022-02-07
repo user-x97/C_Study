a77d023b4d817b9a2aa8b49d32d57c8e4a01a23b
Jeongyong Lee <hua9378@naver.com>
2022-02-07 16:46:37 +0900

    함수

--- chap05_test01/main.c
+++ chap05_test01/main.c
@@ -1,17 +1,32 @@
-//함수 만들기
+/*-----------------------------------------------------------
+                         함수 만들기
+-----------------------------------------------------------*/
+
 #include <stdio.h>
+
+// <반환자료형> <함수명>(<파라미터>){}
 
 int sum(int x, int y)
 {
+	if (x < 0) x = x * -1; // 절대값
+	if (y < 0) y = y * -1;
+
 	int result = x + y;
-	return result; // 결과값 반환
+	return result; // 함수 종료 선언, 결과값 반환
 }
 
-void main(void)
+int main(void)
 {
 	int a = 45, b = 20;
 	int sum_result = 0;
 
 	sum_result = sum(a, b); // 함수 호출
-	printf("a와 b의 합은 %d입니다.\n", sum_result);
+	printf("> a와 b의 합은 %d입니다.\n", sum_result);
+
+	// return 0;
+
+	sum_result = sum(-2, -27); // 함수 호출
+	printf("> -2와 -27의 절대값 합은 %d입니다.\n", sum_result);
+
+	return 0; // 정상종료:0
 }

2d76ca8d44f5d89628de1f5a3f2e4e4d8e380863
Jeongyong Lee <hua9378@naver.com>
2022-02-07 11:24:31 +0900

    chaper1 done

--- chap01_test01/main.c
+++ chap01_test01/main.c
@@ -1,5 +1,14 @@
 // 주석입니다. 주석은 컴파일에 영향이 없습니다.
 /*
 여러줄 주석입니다.
-여러줄로 주석으 ㄹ사용할 수 있습니다.
+여러줄로 주석을 사용할 수 있습니다.
 */
+
+#include <stdio.h>
+
+void main() {
+	// 아래는 두 값을 더한 평균입니다.
+	//double average = (10 + 30) / 2;
+	//printf("%lf\n", average);
+	printf("Hello World!");
+}
